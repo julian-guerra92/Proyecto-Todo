ad5ff4de7e39391959d42e215be94117aa73c075
Julian <julianguerra92@gmail.com>
2022-07-06 14:37:35 -0500

    Ajuste final de la aplicación

--- src/classes/todo.class.js
+++ src/classes/todo.class.js
@@ -1,8 +1,17 @@
 
 
-export class Todo{
+export class Todo {
 
-    constructor(tarea){
+    static fromJson({ id, tarea, completado, creado }) {
+        const tempTodo = new Todo(tarea);
+        tempTodo.id = id;
+        tempTodo.completado = completado;
+        tempTodo.creado = creado;
+
+        return tempTodo;
+    }
+
+    constructor(tarea) {
         this.tarea = tarea;
         this.id = new Date().getTime();
         this.completado = false;
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,7 +1,12 @@
+import { Todo } from '../classes';
+import { todoList } from '../index';
 
 //Referencias en ewl HTML
 const divTodoList = document.querySelector(`.todo-list`);
-
+const txtInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
+const ulFiltros = document.querySelector('.filters');
+const anchoFiltros = document.querySelectorAll('.filtro')
 
 export const crearTodoHtml = (todo) => {
     const htmlTodo = `
@@ -22,3 +27,77 @@ export const crearTodoHtml = (todo) => {
     return div.firstElementChild;
 
 }
+
+//Eventos
+
+//Evento para agregar tarea
+txtInput.addEventListener('keyup', (event) => {
+
+    if (event.keyCode === 13 && txtInput.value.length > 0) {
+
+        console.log(txtInput.value);
+        const nuevoTodo = new Todo(txtInput.value);
+        todoList.nuevoTodo(nuevoTodo);
+
+        crearTodoHtml(nuevoTodo);
+        txtInput.value = '';
+    }
+
+});
+
+//Evento para marcar como hecha una tarea
+divTodoList.addEventListener('click', (event) => {
+
+    const nombreElemento = event.target.localName;
+    const todoElemento = event.target.parentElement.parentElement;
+    const todoId = todoElemento.getAttribute('data-id');
+
+    if (nombreElemento.includes('input')) {//click en el check
+        todoList.marcarCompletado(todoId);
+        todoElemento.classList.toggle('completed');
+
+    } else if (nombreElemento.includes('button')) {//Para eliminar el todo
+        todoList.eliminarTodo(todoId);
+        divTodoList.removeChild(todoElemento);//Para eliminar un elemento del HTML
+    }
+
+});
+
+//Evento para borrar todas la tareas completadas
+btnBorrar.addEventListener('click', () => {
+    todoList.eliminarCompletados();
+    for (let i = divTodoList.children.length - 1; i >= 0; i--) {
+        const elemento = divTodoList.children[i];
+        if (elemento.classList.contains('completed')) {
+            divTodoList.removeChild(elemento);
+        }
+    }
+});
+
+ulFiltros.addEventListener('click', (event) => {
+
+    const filtro = event.target.text;
+    if (!filtro) { return };
+    anchoFiltros.forEach(elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+    for (const elemento of divTodoList.children) {
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch (filtro) {
+            case 'Pendientes':
+                if (completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
+
+            case 'Completados':
+                if (!completado) {
+                    elemento.classList.add('hidden');
+                }
+                break;
+        }
+    }
+
+});
